d67e096ff5fcb6514cfb39532415d78267b252ed
aravindhgithubb <40801991+aravindhgithubb@users.noreply.github.com>
2019-04-04 21:22:12 +0530

    Add files via upload

--- DNBController.java
+++ DNBController.java
@@ -1,63 +1,74 @@
 package com.dnb.controller;
 
 import javax.ws.rs.FormParam;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-
 import com.dnb.model.CustomerDetails;
 import com.dnb.serviceimpl.CustomerDetailsImpl;
 
 @Controller
-@RequestMapping("/customer")
+//@RequestMapping("/customer")
+//@SessionAttributes("name")
 public class DNBController {
 	@Autowired
 	private CustomerDetailsImpl customerDetailsImpl;	
 	
-	@RequestMapping(value="/customerDetails", method = RequestMethod.GET)
-	public @ResponseBody String getCustomerDetails(@FormParam("name") String name,@FormParam("phoneNo") String phoneNo){
+	@RequestMapping(value="/search", method = RequestMethod.GET)
+	
+	public  String getCustomerDetails(Model model,@FormParam("name") String name,@FormParam("phoneNo") String phoneNo){
 		System.out.println("inside customer details");	
-		//String results = " Spring boot application";
-		CustomerDetails simpleModel1 = null;
+		CustomerDetails cusPo=new CustomerDetails();
 		try {
-		simpleModel1= customerDetailsImpl.getCustomerDetails(name,phoneNo);
-		} catch (Exception e){
+			cusPo= customerDetailsImpl.getCustomerDetails(name,phoneNo);
+			if(cusPo !=null) {
+             model.addAttribute("fname",cusPo.getName());
+             model.addAttribute("address",cusPo.getAddress());
+             model.addAttribute("creation",cusPo.getCreationdate());
+             model.addAttribute("phone",cusPo.getPhone());
+             model.addAttribute("post",cusPo.getAccom().getPost());
+             model.addAttribute("source",cusPo.getOwns().getSource());
+             
+           /*  model.addAttribute("fname",cusPo.getName());
+             model.addAttribute("address",cusPo.getAddress());
+             model.addAttribute("creation",cusPo.getCreationdate());
+             model.addAttribute("phone",cusPo.getPhone());
+             model.addAttribute("post",cusPo.getAccom().getPost());
+             model.addAttribute("source",cusPo.getOwns().getSource());*/
+				
+				/*model.addAttribute("Name",cusPo.getCustDet().getName());
+			model.addAttribute("Phone",cusPo.getCustDet().getPhone());
+			model.addAttribute("Address",cusPo.getCustDet().getAddress());
+			model.addAttribute("creationdate",cusPo.getCustDet().getCreationdate());
+		return "model";*/
+		/*	model.setViewName("search");
+				model.addObject("search",cusPo);
+				return model;*/
+				return "newResult";
+          
+ }} catch (Exception e){
 			System.out.println("Exception"+e);
 		}
-		
-		String results = null;
-		if(simpleModel1 !=null) {		
-			results = "ID = "+simpleModel1.getId();
-			results = results + " Name = "+simpleModel1.getName();
-			results = results + " Phone = "+simpleModel1.getPhoneNumber();
-			results = results + " Address = "+simpleModel1.getAddress();
-			System.out.println("Results"+results);	
-				
-		}else {
-			results = "Customer name "+name +" not found in DNB ";
-		}
-		
-	return results;
+		return "search";
 	}
+	
+	
 	@RequestMapping(value="/customerAddDetails", method = RequestMethod.GET)
 	public  @ResponseBody String getCustomerAddDetails(@FormParam("custid") Integer custid, @FormParam("name") String name,@FormParam("phoneNo") String phoneNo,@FormParam("address") String address){
 		System.out.println("inside customer details"+custid);
 		System.out.println("inside customer details"+name);
 		System.out.println("inside customer details"+phoneNo);
 		System.out.println("inside customer details"+address);
-		//String name = "Senthil";
-		CustomerDetails customerDetailsModel = new CustomerDetails();
+        CustomerDetails customerDetailsModel = new CustomerDetails();
 		customerDetailsModel.setId(custid);
 		customerDetailsModel.setName(name);
-		customerDetailsModel.setPhoneNumber(phoneNo);
+		customerDetailsModel.setPhone(phoneNo);
 		customerDetailsModel.setAddress(address);
-		
-		String message = customerDetailsImpl.addCustomerDetails(customerDetailsModel);
-		
-	return message;
+      String message = customerDetailsImpl.addCustomerDetails(customerDetailsModel);
+    return message;
 	}
 	@RequestMapping(value="/customerDeleteDetails", method = RequestMethod.GET)
 	public  @ResponseBody String getCustomerAddDetails(@FormParam("custid") Integer custid){
